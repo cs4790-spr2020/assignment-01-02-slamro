20e9e09382be3407a240404ffdcf21753ec46158
steven lamoreaux <stevenlamoreaux@gmail.com>
2020-02-09 23:41:52 -0700

    added for assignment 3

--- BlabberApp/BlabberApp.DataStoreTest/InMemoryTest.cs
+++ BlabberApp/BlabberApp.DataStoreTest/InMemoryTest.cs
@@ -1,9 +1,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace BlabberApp.DomainTest
+namespace BlabberApp.DataStoreTest
 {
     [TestClass]
-    public class UnitTest1
+    public class InMemoryTest
     {
         [TestMethod]
         public void TestMethod1()
